b0dc1e85965ca4ca2ac2dfac9fcf11f2f184807e
Jorge Alberto Garcia Estrada <alberto-garcia20@outlook.com>
2022-10-04 11:52:31 -0500

    Aprendiendo sobre Generics

---
+++ Generics/Program.cs
@@ -0,0 +1,31 @@
+﻿using Generics.Models;
+using Generics.Service;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Post newPost = new()
+            {
+                UserId = 20,
+                Title = "Generics in C#",
+                Body = "Learning what generics are and how to use its"
+            };
+
+            SendRequest <Post> postService = new SendRequest<Post>();
+            var result = await postService.Send(newPost);
+
+            Todo newTodo = new()
+            {
+                UserId = 20,
+                Title = "Learning about Generics in C#",
+                Completed = false
+            };
+
+            SendRequest<Todo> todoService = new SendRequest<Todo>();
+            var todoResult = await todoService.Send(newTodo);
+        }
+    }
+}
---
+++ Generics/Service/SendRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Generics.Service
+{
+    /* Si queremos delimitar el uso de un servicio debemos hacer uso 
+     * de interfaces en las clases donde vamos a implementar determinado servicio
+     * asi podemos condicionar el uso de determinado servicio.
+     * para hacer esto debemos usar where T : Interface, por ejemplo.
+     * 
+     * public class SendRequest<T> where T:Interface
+     * 
+     * asi solo las clases que implementen determinada interface podran hacer uso de el
+     * servicio SendRequest.
+     */
+    public class SendRequest<T>
+    {
+        private HttpClient _client = new();
+
+        public async Task<T> Send(T model)
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+
+
+            string data = JsonSerializer.Serialize<T>(model);
+            HttpContent content = new StringContent(data, Encoding.UTF8, "application/json");
+            HttpResponseMessage httpResponse = await _client.PostAsync(url, content);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                string result = await httpResponse.Content.ReadAsStringAsync();
+                var postResult = JsonSerializer.Deserialize<T>(result, 
+                    new JsonSerializerOptions() 
+                    { 
+                        PropertyNameCaseInsensitive=true
+                    });
+
+                return postResult;
+            }
+            return default(T);
+        }
+    }
+}

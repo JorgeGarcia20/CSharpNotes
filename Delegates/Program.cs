3b1a16e3e2303a548b53b26cb26936705578fabc
JorgeGarcia20 <alberto-garcia20@outlook.com>
2022-10-05 18:25:15 -0500

    Aprendiendo acerca de delegados en c#

---
+++ Delegates/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Delegates
+{
+    public class Program
+    {
+        /*
+        Delegates
+        Un delegado es un tipo que encapsula de forma segura un método.
+        Normalmente un objeto delegado se construye al proporcionar el nombre
+        del método que el delegado encapsula o con una expresion lambda.
+        Una vez que se crea una instancia de delegado, el delegado pasará al método
+        una llamada de método realizada al delegado. 
+        */
+
+        public delegate void Del(string message);
+
+        public static void Main(string[] args)
+        {
+            Del handler = DelegateMethod;
+            handler("Hello world from a delegate!");
+            MethodWithCallback(100, 299, handler);
+        }
+
+        // this method is going to be to the delegate
+        public static void DelegateMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void MethodWithCallback(int param1, int param2, Del callback)
+        {
+            callback($"The number is: {param1 + param2}");
+        }
+    }
+}
